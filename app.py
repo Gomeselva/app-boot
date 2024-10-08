f63eb336301b13fade47ce00264b17d59de2ae6c
Gomeselva <121693563+Gomeselva@users.noreply.github.com>
2024-10-08 17:20:26 -0400

    traducao para ingles

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, request, jsonify
+from services.waha import Waha
+from bot.ai_bot import AIbot
+import time
+import random
 
 
 app = Flask(__name__)
@@ -7,8 +11,27 @@ app = Flask(__name__)
 def webhook():
     data = request.json
     print(f'EVENTO RECEBIDO: {data}')
+
+    waha = Waha()
+    ai_bot = AIbot
+
+    chat_id = data["payload"]["from"]
+    received_message = data["payload"]["body"]
+
+    waha.start_typing(chat_id=chat_id)
+
+
+    response = ai_bot.invoke(received_message)
+
+    waha.send_message(
+        chat_id=chat_id, 
+        message = response,
+    )
+
+    waha.stop_typing(chat_id=chat_id)
           
-    return jsonify({'status': 'success'})
+    return jsonify({'status': 'success'}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    #app.run(ssl_context=('path/to/cert.pem', 'path/to/key.pem'))
+    app.run(host='0.0.0.0', port=5000, debug=True)	
